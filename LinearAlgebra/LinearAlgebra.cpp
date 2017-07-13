9e4f10eef8b40aa4982e39256d4f1ce6c561ca78
Tristan Charpentier <addict1@outlook.com>
2017-07-12 21:45:59 -0400

    Created constructors for classes

--- LinearAlgebra/LinearAlgebra.cpp
+++ LinearAlgebra/LinearAlgebra.cpp
@@ -2,10 +2,68 @@
 //
 
 #include "stdafx.h"
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+class Matrix {
+public:
+	Matrix(int r, int c) : rows(r), columns(c) {};
+	int rows;
+	int columns;
+	vector<vector<double> > data;
+	bool initialize();
+	Matrix operator+(const Matrix& b);
+};
+
+class MatrixFunctions {
+public:
+	Matrix parse();
+private:
+	Matrix parse_from_string(string input);
+};
+
+Matrix MatrixFunctions::parse()
+{
+	cout << "Enter matrix values separated by commas and by semicolons: \n";
+
+}
+
+Matrix Matrix::operator+(const Matrix &b)
+{
+	Matrix m(rows, columns);
+	m.initialize();
+
+	if (b.rows != rows || b.columns != columns)
+		throw "Dimension mismatch!";
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			m.data[i][j] = b.data[i][j] + data[i][j];
+		}
+	}
+
+	return m;
+}
+
+bool Matrix::initialize()
+{
+	data.resize(rows);
+	for (int i = 0; i < columns; i++)
+	{
+		data[i].resize(columns);
+	}
+	return true;
+}
 
 int main()
 {
+	Matrix m(3, 4);
+	cout << sizeof(m) << "\n";
+	system("pause");
     return 0;
 }
 
